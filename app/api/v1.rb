2e47a84579cc6a5cf7228166e92ac0df7f446e48
Eiji Hachiya <hachiyae@gmail.com>
2024-08-11 15:35:09 +0900

    feat: add user list and user

--- app/api/v1.rb
+++ app/api/v1.rb
@@ -2,4 +2,6 @@
 
 class V1 < Grape::API
   version "v1"
+
+  mount V1::Users
 end
--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -1,22 +1,17 @@
 # frozen_string_literal: true
 
-module V1
-  class Users < Grape::API
-    resource :users do
-      get do
-        users = User.all
-        present users, with: V1::Entities::User, type: 'admin'
-      end
+class V1::Users < Grape::API
+  resource :users do
+    get do
+      User.all
+    end
 
-      params do
-        requires :id, type: Integer, desc: 'User id'
-      end
-      route_param :id do
-        get do
-          user = User.find(params[:id])
-          present user, with: V1::Entities::User, type: 'admin'
-        end
-        mount V1::Users::Comments
+    params do
+      requires :id, type: Integer, desc: "User id"
+    end
+    route_param :id do
+      get do
+        User.find(params[:id])
       end
     end
   end
